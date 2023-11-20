f36e4e34ac7a3e9116242894d1cf9fcfb0278040
katawut <katawutu.io@gmail.com>
2023-11-21 00:21:39 +0700

    create search component and setup hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import {createRoot} from "react-dom/client";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Vincent" animal="Cat" breed="Scottish Fold" />
-  </div>
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
