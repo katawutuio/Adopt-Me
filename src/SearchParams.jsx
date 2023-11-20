1a348b6b1a42336f0944f247194c5875e705ce47
katawut <katawutu.io@gmail.com>
2023-11-21 00:36:37 +0700

    Add animal and mapping through data with hooks

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -2,17 +2,29 @@ import { useState } from "react";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const ANIMALS = ['dog', 'cat', 'bird', 'rabbit', 'penguin']
+  const [animal, setAnimal] = useState("")
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
+          Location
           <input
             id="location"
             onChange={(e) => setLocation(e.target.value)}
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select id="animal" value={animal} onChange={(e) => {setAnimal(e.target.value)}}>
+            <option />
+            {ANIMALS.map((animal) => (
+                <option key={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
