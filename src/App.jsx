2db19270bb487abb57aa87978b4a0602ddbcd504
katawut <katawutu.io@gmail.com>
2023-11-20 21:54:17 +0700

    refactor code and setup eslint for work with React

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Vincent",
-      breed: "Scottish Fold",
-    }),
-  ]);
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Vincent" animal="Cat" breed="Scottish Fold" />
+  </div>
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App />);
