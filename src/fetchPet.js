902a8e167b4d1ca287e716fc34d884d2087cbe86
Katawut <katawut.u@sabuysolutions.com>
2023-11-28 09:54:06 +0700

    setup react query

---
+++ src/fetchPet.js
@@ -0,0 +1,13 @@
+const fetPetch = async ({queryKey}) => {
+  const id = queryKey[1];
+
+  const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+
+  if (!apiRes.ok) {
+    throw new Error(`detail/${id} fetch not ok`);
+  }
+
+  return apiRes.json();
+}
+
+export default fetPetch;
